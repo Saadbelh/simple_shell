8ee6a1de214eeab5a5e3af54d68cbecc843dd335
Saadbelh <saad.belhimer@gmail.com>
2023-10-18 03:22:39 -0700

    ttrtrr

---
+++ shell.h
@@ -0,0 +1,39 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+#define DELIM " \n\t"
+
+extern char **environ;
+
+
+char *read_line(void);
+char **tokenizer(char *line);
+int _execute(char **command, char **arv, int idx);
+char *_getenv(char *variable);
+char *_getpath(char *command);
+
+
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+
+void freearray2D(char **array);
+void print_error(char *name, char *cmd, int idx);
+
+#endif
