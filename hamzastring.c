099cb851243ad39ab74e79b390c402bc3aeb8d08
Saadbelh <saad.belhimer@gmail.com>
2023-10-18 05:12:58 -0700

    kjojojojojj

---
+++ hamzastring.c
@@ -0,0 +1,110 @@
+#include "shell.h"
+
+/**
+ * _strdup - line first line
+ * @str: pointer
+ *
+ * Return: ptr
+ */
+char *_strdup(const char *str)
+{
+	char *ptr;
+	int i, lan = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
+/**
+ * _strcmp - string
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: cmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int cmp;
+
+	cmp = (int)*s1 - (int)*s2;
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s2++;
+		S1++;
+		cmp = (int)*s1 - (int)*s2;
+	}
+	return (cmp);
+}
+
+/**
+ * _strlen - lenght
+ * @s: pointer
+ *
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcat - cat string
+ * @dest: s
+ * @src: a
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+		p++;
+
+	while (*src)
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = *src;
+	return (dest);
+}
+
+/**
+ * _strcpy - copy  string
+ * @dest: string 1
+ * @src: strig 2
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
