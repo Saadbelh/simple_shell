3f8375b55b7cb8b53871fe631b5ea1cbdd050df7
Medhamza93 <medhamzalahnin@gmail.com>
2023-10-18 03:45:05 -0700

    execution

--- execution.c
+++ execution.c
@@ -1,23 +1,39 @@
 #include "shell.h"
-int _executr(char **command, char **argv)
+/**
+ * _execute - Execution of the shell
+ * @command: Command
+ * @argv: Arguments
+ * @idx: Index
+ * Return: status
+ */
+int _execute(char **command, char **argv, int idx)
 {
+	char *full_cmd;
 	pid_t cild;
 	int status;
+
+	full_cmd = _getpath(command[0]);
+	if (!full_cmd)
+	{
+		print_error(argv[0], command[0], idx);
+		freearray2D(command);
+		return (127);
+	}
 
 	child = fork();
 	if (child == 0)
 	{
-		if (execve(command[0], command, environ) == -1)
+		if (execve(full_cmd, command, environ) == -1)
 		{
-			perror(argv[0]);
+			free(full_cmd, full_cmd = NULL;
 			freearray2D(command);
-			exit(0);
 		}
 	}
 	else
 	{
 		waitpid(child, &status, 0);
 		freearray2D(command);
+		free(full_cmd), full_cmd = NULL;
 	}
 	return (WEXITSTATUS(status));
 }
