c4dfa1694649e52f91d0f516d26622ddb1636f7b
Saadbelh <saad.belhimer@gmail.com>
2023-10-18 17:10:40 -0700

    iiyuyytytt

--- hamzabultin.c
+++ hamzabultin.c
@@ -10,8 +10,10 @@
 
 void handle_builtin(char **command, char **argv, int *status, int idx)
 {
+	(void) argv;
+	(void) idx;
 	if (_strcmp(command[0], "exit") == 0)
-		exit_shell(command, argv, status, idx);
+		exit_shell(command, status);
 
 	else if (_strcmp(command[0], "env") == 0)
 		print_env(command, status);
